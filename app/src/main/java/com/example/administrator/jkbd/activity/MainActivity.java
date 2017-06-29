b2b2849b4d8d38026eaa05f91f3350d58db9a97e
QinFufeng <1870205385@qq.com>
2017-06-29 17:02:22 +0800

    test

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.jkbd.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.administrator.jkbd.R;
 
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void test(View view) {
     }
 }
