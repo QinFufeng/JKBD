3002712bd05c72e1b9dc637bb941157b0446e636
QinFufeng <1870205385@qq.com>
2017-06-30 00:38:32 +0800

    修复exam界面bug

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.jkbd.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
+        /*Intent intent=new Intent(MainActivity.this,ExamActivity.class);
+        startActivity(intent) ;*/
+
+    }
+
+    public void exit(View view) {
+        finish();
     }
 }
