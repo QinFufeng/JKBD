b08b57559376af8cc88677dc189ea20dbcdf244c
QinFufeng <1870205385@qq.com>
2017-06-29 16:50:03 +0800

    exam界面完成一半

--- app/src/main/java/com/example/administrator/jkbd/bean/Examination.java
+++ app/src/main/java/com/example/administrator/jkbd/bean/Examination.java
@@ -54,7 +54,7 @@ public class Examination {
     @Override
     public String toString() {
         return "考试科目:" + subjectTitle +"\n" +
-                "考试数量:" + questionCount +"\n" +
+                "考题数量:" + questionCount +"\n" +
                 "考试时间:" + limitTime +"分钟";
     }
 }
